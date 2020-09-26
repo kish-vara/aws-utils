51642da8ac13f5b0eecdaa31e534dcc6462ea46c
kish-vara <kishan_vara@hotmail.co.uk>
2020-09-26 02:43:14 +0100

    Initial mvn support + dummy classes

---
+++ s3-utils/src/test/java/com/utils/DynamoUtilsTest.java
@@ -0,0 +1,27 @@
+package com.utils;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DynamoUtilsTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+
+    @Test
+    public void exampleTest(){
+
+        Assert.assertEquals(true, true);
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
